821cb5c490a55b87508b463f0ad9adb3fbc9bbae
costar011 <dnaer456@gmail.com>
2024-10-11 00:04:11 +0900

    webpack errr

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,47 @@
-"use strict";
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
-  entry: {
-    main: ["./src/main.js"],
-  },
-
-  node: {
-    fs: "empty",
-    net: "empty",
-  },
-
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./build"), //  resolve(__dirname) -> 현재 내 파일의 위치 , "./build" =>
-    filename: "[name].js",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "build"),
+    index: "index.html",
+    port: 3000,
+    proxy: {
+      '/user_inform': {
+        target: 'http://localhost:3001/',
+        changeOrigin: true,
+      }
+    }
+  },
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: path.resolve(__dirname, "./src"),
-        loader: "babel-loader",
+        exclude: "/node_modules",
+        use: ['babel-loader'],
       },
       {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-      },
-      {
-        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+        test: /\.html$/,
         use: [
           {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              fallback: "file-loader",
-              name: "images/[name].[ext]", // images안에서만 인식할 수 있다 파일을.
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              fallback: "file-loader",
-              name: "fonts/[name].[ext]", // fonts는 fonts안에서만 인식
-            },
-          },
-        ],
-      },
-    ],
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-    new CopyWebpackPlugin([
-      {
-        context: "./public", // public 안에 있는 index html 인식
-        from: "*.*",
-      },
-    ]),
-    new Dotenv(), // Dotenv사용 할 것 이다.
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
   ],
- devServer: {
-    contentBase: path.resolve("./build"),
-    index: "index.html",
-    // 클라이언트 포트는 3000
-    port: 3000,
-    proxy: {
-      '/user_inform': {
-      	// 서버 포트는 3001
-        target: 'http://localhost:3001/', // proxy : 서포터 역할   // ** : 모든 파일, 모든 폴더
-        changeOrigin: true, 
-      }
-    }
-  },
-  devtool: "eval-source-map",
+  devtool: "eval-source-map"
 };
